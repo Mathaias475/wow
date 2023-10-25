1a04abec5d5e11d551efce1bbfd3296d3801a960
Matias Sammartino <matias@techforb.com>
2023-10-24 21:12:16 -0300

    CHG: WOW: ARG: git-ignore modifies

--- src/app/common/services/items.service.ts
+++ src/app/common/services/items.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from "@angular/core";
 import { CacheStore } from "src/app/wow/interfaces/cacheStore.interface";
-import { HttpClient } from '@angular/common/http';
 import { Item } from "src/app/wow/interfaces/item.interface";
-import { map, tap } from "rxjs";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "firebase.config";
-import { DataSnapshot, child, get, getDatabase, ref } from 'firebase/database';
-
-
+import { child, get, getDatabase, ref } from "firebase/database";
 
 @Injectable({ providedIn: "root" })
 export class ItemsService {
 
-private itemsUrl = firebaseConfig.databaseURL;
-
-private app = initializeApp(firebaseConfig);
-private db = getDatabase(this.app);
-
-
-constructor(private http: HttpClient) { }
-
-
+  private app = initializeApp(firebaseConfig);
+  private db = getDatabase(this.app);
 
   cacheStore: CacheStore = {
     byItem: {
@@ -32,20 +21,21 @@ constructor(private http: HttpClient) { }
   async getItems(term: string): Promise<Item[] | void> {
     term = term.toLowerCase();
     const dbref = ref(this.db);
-    
+
     try {
-      const snapshot = await get(child(dbref, '/Items'));
+      const snapshot = await get(child(dbref, "/Items"));
       const data = snapshot.val();
-  
+
       if (data) {
-        const filteredItems = data.filter((res: Item) => res.name.toLowerCase().includes(term));
+        const filteredItems = data.filter((res: Item) =>
+          res.name.toLowerCase().includes(term)
+        );
         this.cacheStore.byItem.items = filteredItems;
         this.cacheStore.byItem.term = term;
         return filteredItems;
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   }
-
 }
